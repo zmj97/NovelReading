2c14977306f316d5ee77950dd874a2ea820ee975
zmj97 <295692590@qq.com>
2019-11-13 19:26:43 +0800

    初始版完成

---
+++ README.md
@@ -0,0 +1,30 @@
+# novel_reading
+![pub](https://img.shields.io/pub/v/novel_reading)
+一个支持上拉加载更多和缓存的小说阅读Flutter插件。
+A Flutter novel reading package supports pull-up loading more and caching.
+
+## How to Use
+
+```dart
+NovelReading(
+    theme: NovelReadingType.green, // use provided theme [white(default), black, green, pink]
+    start: 5, // readding novel starts from chapter 5
+    cache: 10, // cache 10 chapters
+    color: , // setting background color
+    textStyle: , // setting text style
+    builder: (page) async {
+        return await http.get('example.com&page=$page');
+    },
+)
+
+```
+
+---
+This project is a starting point for a Dart
+[package](https://flutter.dev/developing-packages/),
+a library module containing code that can be shared easily across
+multiple Flutter or Dart projects.
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.dev/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/novel_reading.dart
@@ -0,0 +1,147 @@
+library novel_reading;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+enum NovelReadingTheme {
+  white,
+  green,
+  pink,
+  black
+}
+
+class NovelReading extends StatefulWidget {
+
+  // 从第几页开始
+  // start from which page
+  final int start;
+
+  // 预加载页数
+  // how much pages to preload
+  final int cache;
+
+  // 如何获取第page页的数据，返回字符串
+  // how to get a page's data, return string
+  final Future<String> Function(int page) builder;
+
+  // 字体样式
+  // novel text font style
+  final TextStyle textStyle;
+
+  // 背景色
+  //background color
+  final Color color;
+
+  final NovelReadingTheme theme;
+
+  NovelReading({Key key, this.start = 0, this.cache = 5, @required this.builder, this.textStyle, this.color, this.theme = NovelReadingTheme.white}) : super(key: key);
+
+  @override
+  _NovelReadingState createState() => _NovelReadingState();
+}
+
+class _NovelReadingState extends State<NovelReading> {
+
+  get start => widget.start;
+  get cache => widget.cache;
+  get builder => widget.builder;
+  get textStyle => widget.textStyle;
+  get color => widget.color;
+  get theme => widget.theme;
+
+  int currentPage;
+
+  bool _showLoading = false;
+
+  String _novel = '';
+
+  ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initNovel();
+
+    _controller = new ScrollController()
+      ..addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        currentPage += cache + 1;
+        _loadMore(currentPage, cache);
+      }});
+  }
+
+  void _loadMore(int from, int cache) async {
+    setState(() {
+      _showLoading = true;
+    });
+    for (int i = from; i <= from + cache; i++) {
+      _novel += (await builder(i));
+    }
+    setState(() {
+      _showLoading = false;
+    });
+  }
+  
+  void _initNovel() async {
+    currentPage = start;
+    _loadMore(start, cache);
+  }
+
+  TextStyle _getTextStyle() {
+    if (textStyle != null) {
+      return textStyle;
+    }
+    if (theme == NovelReadingTheme.black) {
+      return TextStyle(
+        color: Colors.white,
+        height: 1.75
+      );
+    }
+    // default text color is black
+    return TextStyle(
+      color: Colors.black,
+      height: 1.75
+    );
+  }
+
+  Color _getBgcolor() {
+    if (color != null) {
+      return color;
+    }
+    if (theme == NovelReadingTheme.green) {
+      return Colors.green[300];
+    }
+    if (theme == NovelReadingTheme.pink) {
+      return Colors.pink[50];
+    }
+    if (theme == NovelReadingTheme.black) {
+      return Colors.black87;
+    }
+    // default background color is black
+    return Colors.white;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: _getBgcolor(),
+          padding: EdgeInsets.only(top: 12, left: 12, right: 12),
+          child: SingleChildScrollView(
+            child: Text(_novel, style: _getTextStyle(),),
+            controller: _controller,
+          ),
+        ),
+        Opacity(
+          opacity: _showLoading ? 1 : 0,
+          child: Center(
+            child: CupertinoActivityIndicator(),
+          )
+        )
+      ],
+    );
+  }
+}
